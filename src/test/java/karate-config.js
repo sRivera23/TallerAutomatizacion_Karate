2d5a7fa01c93acf7419f4c8cbfdcbb90e7bc53a7
chrisystems <51771601+chrisystems@users.noreply.github.com>
2025-06-26 18:25:28 -0500

    Update karate-config.js

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -7,11 +7,11 @@ function fn() {
     var server = '192.168.0.182:8080';
     if (karate.env == 'prod') {
         protocol = 'https';
-        server = 'parabank.parasoft.com';
+        server = 'thinking-tester-contact-list.herokuapp.com';
     }
 
     var config = {
-        baseUrl: protocol + '://' + server + '/parabank/services/bank'
+        baseUrl: protocol + '://' + server,
     };
     config.faker = Java.type('com.github.javafaker.Faker');
 
